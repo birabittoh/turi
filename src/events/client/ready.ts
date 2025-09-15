ce3f8573946bfe8eee49f22f015e3cb8d2fa78e8
Marco Andronaco <andronacomarco@gmail.com>
2025-09-15 02:01:58 +0200

    Set activity

--- src/events/client/ready.ts
+++ src/events/client/ready.ts
@@ -1,10 +1,12 @@
 import { ClientEvent } from "../..";
-import { type Client, Routes } from "discord.js";
+import { ActivityType, type Client, Routes } from "discord.js";
 
 export default class ErrorEvent extends ClientEvent<"clientReady"> {
   readonly name = "clientReady";
   async run(c: Client<true>) {
     console.log(`Logged in as ${c.user.tag}!`);
+
+    c.user.setActivity("FOSS", { type: ActivityType.Competing });
 
     await c.rest.put(Routes.applicationCommands(c.user.id), {
       body: this.client.commands.map(c => c.slashBuilder.toJSON()),
